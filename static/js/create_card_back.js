d4d6576830b867a4d453d436597de3f0b7cd4ea2
liao1030 <liao1030@gmail.com>
2025-02-27 16:37:59 +0800

    將卡片背面改為黑色，文字改為黃色

--- static/js/create_card_back.js
+++ static/js/create_card_back.js
@@ -6,23 +6,23 @@ const { createCanvas } = require('canvas');
 const canvas = createCanvas(200, 300);
 const ctx = canvas.getContext('2d');
 
-// 繪製卡牌背景（紅色）
-ctx.fillStyle = '#D50000';
+// 繪製卡牌背景（黑色）
+ctx.fillStyle = '#000000';
 ctx.fillRect(0, 0, 200, 300);
 
 // 繪製卡牌邊框
-ctx.strokeStyle = '#000000';
+ctx.strokeStyle = '#333333';
 ctx.lineWidth = 5;
 ctx.strokeRect(5, 5, 190, 290);
 
 // 繪製UNO標誌橢圓
-ctx.fillStyle = '#FFFFFF';
+ctx.fillStyle = '#222222';
 ctx.beginPath();
 ctx.ellipse(100, 150, 70, 100, Math.PI / 4, 0, 2 * Math.PI);
 ctx.fill();
 
 // 繪製UNO文字
-ctx.fillStyle = '#D50000';
+ctx.fillStyle = '#FFFF00';
 ctx.font = 'bold 60px Arial';
 ctx.textAlign = 'center';
 ctx.textBaseline = 'middle';
